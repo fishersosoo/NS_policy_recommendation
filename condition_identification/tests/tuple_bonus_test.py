d6c0cb8e39522268a64110ce9e90a80d0b539200
KODGV <851921521@qq.com>
2018-11-19 19:43:22 +0800

    增加了get_tree函数 构造函数传入的文件名移动到具体构建树的时候再传 增加了一些注释调整 把tests中tuple_bonus_test.py中涉及到的tree的获取更改

--- condition_identification/tests/tuple_bonus_test.py
+++ condition_identification/tests/tuple_bonus_test.py
@@ -7,8 +7,8 @@ from bonus_identify.Tree import DocTree
 from predicate_extraction.tuple_bonus_recognize import TupleBonus
 
 def test_subtree():
-    tree=DocTree('../bonus_identify/广州南沙新区(自贸片区)促进总部经济发展扶持办法｜广州市南沙区人民政府.txt')
-    tree.construct()
+    tree=DocTree()
+    tree.construct('../bonus_identify/广州南沙新区(自贸片区)促进总部经济发展扶持办法｜广州市南沙区人民政府.txt')
     tuplebonus = TupleBonus()
     tuplebonus.bonus_tuple_analysis(tree)
     tuplebonus.get_bonus_tree().show()
