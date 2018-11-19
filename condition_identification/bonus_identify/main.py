ef452f7c13e241f7335b12ffa1152de18c05f4f6
KODGV <851921521@qq.com>
2018-11-19 19:35:46 +0800

    # 增加了get_tree函数 # 构造函数传入的文件名移动到具体构建树的时候再传 # 增加了一些注释调整

--- condition_identification/bonus_identify/main.py
+++ condition_identification/bonus_identify/main.py
@@ -2,8 +2,8 @@
 import re
 from Tree import DocTree
 if __name__ == '__main__':
-    tree=DocTree('广州南沙新区(自贸片区)促进总部经济发展扶持办法｜广州市南沙区人民政府.txt')
-    tree.construct()
+    tree=DocTree()
+    tree.construct('广州南沙新区(自贸片区)促进总部经济发展扶持办法｜广州市南沙区人民政府.txt')
     print(tree.get_bonus_nodes())
 
 
