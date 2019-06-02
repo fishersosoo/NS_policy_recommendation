d56014a9a8e845ca2b4a148fb73138bc5eb2abed
KODGV <851921521@qq.com>
2019-06-02 19:30:04 +0800

    perf(database_parse): 提升数据库筛选速度
    1.重构了代码
    2.将操作向量化
    3.删除多余的Print语句

--- ns_ai_system/condition_identification/database_process/database_parse.py
+++ ns_ai_system/condition_identification/database_process/database_parse.py
@@ -1,25 +1,19 @@
 # coding=utf-8
-from condition_identification.name_entity_recognition.extract_keyword import *
 from condition_identification.util.distance_calculations import cos_sim
 from condition_identification.rule.adjust_database_keyword import adjust_database_keyword_byrule
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
+
 from condition_identification.args import database_cluster_quantile
 from condition_identification.args import database_cluster_max_length
 from condition_identification.args import database_cluster_min_length
 from condition_identification.args import database_cluster_similarity
 from condition_identification.name_entity_recognition.vectorize.bert_word2vec import BertClient
-
-def data_filter(database_value):
-    bert_client = BertClient()
-    values_set = set(database_value)
-    values_set = database_extract(values_set, bert_client)
-    return values_set
+from condition_identification.util.sentence_preprocess import filter_punctuation_include_content
+from collections import defaultdict
 
 
-
-def database_extract(database_values, bc, max_length=-1, len_threshold=1):
+def database_extract(database_values):
     """对数据库的列值进行抽取
 
     数据库的列值进行抽取,
@@ -32,16 +26,29 @@ def database_extract(database_values, bc, max_length=-1, len_threshold=1):
     Returns:
         database_cluster_result: list ,聚合处理后的数据库的值
     """
-    database_keywords = []
+
+    database_keywords = set()
     for index, database_value in enumerate(database_values):
-        if max_length != -1 and index > max_length:     # max_length不是-1，抽取前max_length行
+        if len(database_keywords) > database_cluster_max_length:
             break
-        database_keywords.extend(extract_keyword(database_value, len_threshold))
-    database_cluster_result = database_cluster(database_keywords, bc)
+        database_keywords = database_keywords.union(set(extract_databaseword(database_value)))
+
+    if len(database_keywords)< database_cluster_min_length:
+        database_cluster_result = list(database_keywords)
+    else:
+        database_cluster_result = database_cluster(database_keywords)
     return database_cluster_result
 
 
-def database_cluster(database_keywords, bc):
+def extract_databaseword(database_value):
+    database_value = filter_punctuation_include_content(database_value)
+    database_value.replace('，', ";")
+    database_value = database_value.split(';')
+    database_value = list(filter(None,database_value))
+    return database_value
+
+
+def database_cluster(database_keywords):
     """对数据库列数据聚类
 
     对列数据很多的列进行聚类，从而剔除无关值
@@ -53,23 +60,19 @@ def database_cluster(database_keywords, bc):
          cluster_word: list 返回抽取后的列值
 
      """
-    tqdm.pandas()
-    # # 获得列数据的词向量 以及 对列数据去重
-    database_keyword_vecs = bc.encode(database_keywords)
-    # 如果数据太少就没有清除的必要
-    database_keywords_len = len(database_keywords)
-    if database_keywords_len < database_cluster_min_length:    # 数据过少不做后续处理直接返回
-        return database_keywords
-    elif database_keywords_len > database_cluster_max_length:
-        database_keyword_vecs = database_keyword_vecs[0:database_cluster_max_length]
-        database_keywords = database_keywords[0:database_cluster_max_length]
+    bert_client = BertClient()
+
     database_keywords = adjust_database_keyword_byrule(database_keywords)
+    database_keyword_vecs = bert_client.encode(database_keywords)
     # 循环计算相似度
-    count_dict = {keyword: 0 for keyword in database_keywords}
-    for index, database_keyword in tqdm(enumerate(database_keywords)):
-        for i in range(index+1, database_keywords_len):
-            if cos_sim(database_keyword_vecs[index], database_keyword_vecs[i]) > database_cluster_similarity:
-                count_dict[database_keyword] += 1
+    count_dict = defaultdict(int)
+    for index, database_keyword in enumerate(database_keywords):
+        vec2s = np.array(database_keyword_vecs[index+1:])
+        if len(vec2s)==0:
+            break
+        vec1 = np.array(database_keyword_vecs[index])
+        count_dict[database_keyword]=np.sum(np.apply_along_axis(cos_sim, 1, vec2s, vec1) > database_cluster_similarity)
+
 
     # 取前0.4的数值
     count_dict = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
@@ -79,7 +82,13 @@ def database_cluster(database_keywords, bc):
 
 
 if __name__ == '__main__':
-    words = ['1']*1000
-    with open('F:\\txt\\txt\\2.txt', 'r', encoding='utf8')as rf:
-        for line in rf:
-            words.append(line.strip())
+    a = [[1,2],[2,3],[4,5]]
+    b = [3,4]
+    a = np.array(a)
+    b = np.array(b)
+    print(a[3:])
+    print(np.apply_along_axis(cos_sim, 1, a,b)>0.5)
+    print(np.sum(np.apply_along_axis(cos_sim, 1, a,b)>0.5))
+
+
+
