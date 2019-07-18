8940d261a9bdf8b3f9c9b034459624022c332b71
fishersosoo <fishersosoo@outlook.com>
2019-07-18 16:44:30 +0800

    feat(celery, restful): 修改政策推荐接口
    1. 推荐结果不满足、满足、未识别条件分开
    2. 推荐结果不再进行格式化

--- ns_ai_system/celery_task/policy/tasks.py
+++ ns_ai_system/celery_task/policy/tasks.py
@@ -2,6 +2,7 @@
 import copy
 import datetime
 import traceback
+from enum import Enum, unique
 
 import requests
 from celery.result import allow_join_result
@@ -15,6 +16,16 @@ from service import conert_ch2num
 from service.policy_graph_construct import understand_guide
 
 
+@unique
+class MatchResult(Enum):
+    """
+    表示匹配结果
+    """
+    MISMATCH = 0
+    MATCH = 1
+    UNRECOGNIZED = 2
+
+
 # @celery_app.task(rate_limit="2/h")
 @celery_app.task
 def understand_guide_task(guide_id, text):
@@ -26,7 +37,7 @@ def understand_guide_task(guide_id, text):
     understand_guide(guide_id, text)
 
 
-@celery_app.task(soft_time_limit=30)
+@celery_app.task(soft_time_limit=60)
 def check_single_guide(company_id, guide_id, threshold=.0):
     """
     检查单个指南和企业的匹配信息，如果存在匹配则存放到数据库中
@@ -35,6 +46,9 @@ def check_single_guide(company_id, guide_id, threshold=.0):
     :return:
     """
     try:
+        record = {"mismatch": [], "match": [], "unrecognized": []}
+        cached_data = dict()
+        mismatched_sentence_id = set()
         matched_sentence_id = set()
         ret = py_client.ai_system["parsing_result"].find_one({"guide_id": str(guide_id)})
         if ret is None:
@@ -42,34 +56,35 @@ def check_single_guide(company_id, guide_id, threshold=.0):
             return None
         sentences = ret.get("sentences", None)
         triples = ret["triples"]
-        reasons = []
-        fail_to_check = 0
-        checked_fields = []
         for triple in triples:
             if len(triple["fields"]) == 0:
                 continue
-            if triple["fields"][0] in checked_fields:
-                continue
-            match, reason = check_single_requirement(company_id, triple)
-            if match is None:
-                fail_to_check += 1
-            else:
-                checked_fields.append(triple["fields"][0])
-            if match:
-                reasons.append(f'{len(reasons)+1}. {triple["sentence"]}【{reason}】\n')
-                if "sentence_id" in triple:
-                    matched_sentence_id.add(triple["sentence_id"])
-        if sentences is not None:
-            ids = list(sentences.keys())
-            for id in ids:
-                if sentences[id] == "":
-                    sentences.pop(id)
-            mismatched_sentence_id = set(sentences.keys()) - matched_sentence_id
-            log.info(mismatched_sentence_id)
+            match, data, cached_data = check_single_requirement(company_id, triple, cached_data)
+            if match == MatchResult.MISMATCH:
+                if triple["sentence_id"] not in matched_sentence_id:
+                    mismatched_sentence_id.add(triple["sentence_id"])
+            if match == MatchResult.MATCH:
+                if triple["sentence_id"] not in matched_sentence_id:
+                    record["match"].append(
+                        {"sentence": triple["sentence"]})
+                # record["match"].append(
+                #     {"sentence": triple["sentence"], "data": {"field": triple["fields"][0], "value": data}})
+                matched_sentence_id.add(triple["sentence_id"])
+                if triple["sentence_id"] in mismatched_sentence_id:
+                    mismatched_sentence_id.remove(triple["sentence_id"])
+        unrecognized_sentence_id = set(sentences.keys()) - matched_sentence_id - mismatched_sentence_id
+        for one in unrecognized_sentence_id:
+            record["unrecognized"].append(sentences[one])
+        for one in mismatched_sentence_id:
+            record["mismatch"].append(sentences[one])
+        record["company_id"] = company_id
+        record["guide_id"] = guide_id
+        record["time"] = datetime.datetime.utcnow()
+        if (len(record["mismatch"]) + len(record["match"])) == 0:
+            record["score"] = 0
         else:
-            mismatched_sentence_id = None
-        record = format_record(company_id, len(triples) - fail_to_check, guide_id, reasons, sentences,
-                               mismatched_sentence_id)
+            record["score"] = len(record["match"]) / (len(record["mismatch"]) + len(record["match"]))
+        record["latest"] = True
         py_client.ai_system["recommend_record"].delete_many({"company_id": company_id,
                                                              "guide_id": guide_id})
         py_client.ai_system["recommend_record"].insert_one(copy.deepcopy(record))
@@ -102,48 +117,61 @@ def format_record(company_id, count, guide_id, reasons, sentences, mismatched_se
     return record
 
 
-def check_single_requirement(company_id, triple):
+def check_single_requirement(company_id, triple, cached_data):
     """
     检查企业是否满足单一条件
-    :param company_id: 企业id
-    :return: match, reason. match表示是否满足，reason，如果不满足则原因为None
+
+    Args:
+        cached_data: 缓存的数据
+        company_id: 企业id
+        triple: 三元组
+
+    Returns:
+        MatchResult, Data, cached_data
+        MatchResult类表示是否满足或者未识别.
+        Data，表示企业真实数据
     """
+
     if "独立法人资格" in triple["value"]:
-        return True, "是"
-    if len(triple["fields"]) == 0:
-        return None, None
+        return MatchResult.MATCH, "", cached_data
     field = triple["fields"][0]
     field_info = py_client.ai_system["field"].find_one({"item_name": field})
     if field_info is None:
         log.info(f"no field {field}")
-        return None, None
-    # query_data
-    ip = config.get('data_server', 'host')
-    url = f"http://{ip}:{config.get('data_server','port')}/data"
-    server = ServiceProxy(service_url=url)
-    return_data = server.data.sendRequest(company_id, f"{field_info['resource_id']}.{field_info['item_id']}")
-    data = return_data.get("result", None)
+        return MatchResult.UNRECOGNIZED, "", cached_data
+    data = cached_data.get(f"{field_info['resource_id']}.{field_info['item_id']}", None)
+    if data is None:
+        # query_data
+        start_t = datetime.datetime.utcnow()
+        ip = config.get('data_server', 'host')
+        url = f"http://{ip}:{config.get('data_server','port')}/data"
+        server = ServiceProxy(service_url=url)
+        return_data = server.data.sendRequest(company_id, f"{field_info['resource_id']}.{field_info['item_id']}")
+        data = return_data.get("result", None)
     if data is None:
         log.info(f"{return_data}")
-        return None, None
+        return MatchResult.UNRECOGNIZED, "", cached_data
+    else:
+        cached_data[f"{field_info['resource_id']}.{field_info['item_id']}"] = data
     if triple["relation"] in ["大于", "小于"]:
-        return compare_literal(data, triple)
+        match, data = compare_literal(data, triple)
+        return match, data, cached_data
     else:
         if triple["relation"] == "位于":
             if triple["value"] in data[0]:
-                return True, data[0]
+                return MatchResult.MATCH, data[0], cached_data
             else:
-                return False, data[0]
+                return MatchResult.MISMATCH, data[0], cached_data
         if triple["relation"] == "否":
             if triple["value"] not in data[0]:
-                return True, data[0]
+                return MatchResult.MATCH, data[0], cached_data
             else:
-                return False, data[0]
+                return MatchResult.MISMATCH, data[0], cached_data
         if triple["relation"] == "是":
             if triple["value"] in data[0]:
-                return True, data[0]
+                return MatchResult.MATCH, data[0], cached_data
             else:
-                return False, data[0]
+                return MatchResult.MISMATCH, data[0], cached_data
 
 
 def compare_literal(data, triple):
@@ -155,22 +183,22 @@ def compare_literal(data, triple):
             pass
     if len(query_values) == 0:
         log.info(f"can not convert value of {triple['fields'][0]} to float")
-        return None, None
+        return MatchResult.UNRECOGNIZED, ""
     try:
         value = conert_ch2num(triple["value"])
     except:
         log.info(f"can not convert value \"{triple['value']}\" to float")
-        return None, None
+        return MatchResult.UNRECOGNIZED, ""
     if triple["relation"] == "大于":
         for query_value in query_values:
             if query_value > value:
-                return True, query_value
-        return False, query_values[0]
+                return MatchResult.MATCH, query_value
+        return MatchResult.MISMATCH, query_values[0]
     else:
         for query_value in query_values:
             if query_value < value:
-                return True, query_value
-        return False, query_values[0]
+                return MatchResult.MATCH, query_value
+        return MatchResult.MISMATCH, query_values[0]
 
 
 def infer_field_info_from_object_type(object):
@@ -207,8 +235,8 @@ def recommend_task(company_id, guide_id, threshold=.0):
     # results = []
     # for guide in guides:
     result = check_single_guide(company_id, guide_id)
-        # if result is not None:
-            # results.append(result)
+    # if result is not None:
+    # results.append(result)
     # task_group = group([check_single_guide.s(company_id, guide) for guide in guides])
     # 异步执行
     # result = job.apply_async()
@@ -219,12 +247,12 @@ def recommend_task(company_id, guide_id, threshold=.0):
 
 def is_above_threshold(result, threshold):
     try:
-        return float(result["matching"]) >= float(threshold)
+        return float(result["score"]) >= float(threshold)
     except:
         return False
 
 
-@celery_app.task(bind=True, default_retry_delay=300, max_retries=0,soft_time_limit=60)
+@celery_app.task(bind=True, default_retry_delay=300, max_retries=0, soft_time_limit=60)
 def check_single_guide_batch_companies(self, companies, threshold, guide_id, url):
     """
     创建多个子任务检查企业是否满足，阻塞获取所有任务结果
