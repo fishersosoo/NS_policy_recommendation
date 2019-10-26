b9ad449baf9eb61f70f11f04d3c1c8161b9c81ea
KODGV <851921521@qq.com>
2019-10-26 19:05:08 +0800

    feat(rule): 新增人工过滤，把变更事项相关的field过滤
    提供自定义不想要的field作过滤

--- ns_ai_system/condition_identification/rule/adjust_triple.py
+++ ns_ai_system/condition_identification/rule/adjust_triple.py
@@ -1,9 +1,27 @@
 error_cases = ['办公地址', '其他违法记录', '申报企业','统计关系','财务']
-
+field_cases = ['变更事项','变更事项代码','变更前内容','变更后内容','变更时间']
 
 def adjust_byrule(triple):
+    flag1 = adjust_byvalue(triple)
+    flag2 = adjust_byfield(triple)
+    if flag1 or flag2 :
+        return None
+    else:
+        return triple
+
+
+def adjust_byvalue(triple):
+    flag = False
     for error_case in error_cases:
         if error_case in triple.value:
-            triple.fields = None
+            flag = True
             break
-    return triple
+    return flag
+def adjust_byfield(triple):
+    flag = False
+    for field_case in field_cases:
+        for field in triple.fields:
+            if field_case in field:
+                flag = True
+                break
+    return flag
