06fdaa963e56e3dfe9a7843ebd64660fe5173667
KODGV <851921521@qq.com>
2019-11-15 20:44:09 +0800

    feat(condition_identification): 国标行业改为从数据库读取
    1.datamangement新增数据库存取接口
    2.将初始国标存入数据库，小类只存前五个，存代码在另一个项目中

--- ns_ai_system/condition_identification/industry_filter/newindustry.py
+++ ns_ai_system/condition_identification/industry_filter/newindustry.py
@@ -1,20 +1,14 @@
 import os
-import pickle
+from condition_identification.args import INDUSTRY_LIST
 from condition_identification.util.string_process import *
+from data_management.api.industry_standard import *
 def industryFilter(document):
-    print('industryFilter')
     result = titleFilter(document)
     result.extend(contentFileter(document))
-    # if len(result)==0:
-    #     result.extend(similarFilter(document))
     result=nameProcess(result)
     result = list(set(result))
-    if '农、林、牧、渔业' in result:
-        result.remove('农、林、牧、渔业')
     if '居民服务、修理和其他服务业' in result:
         result.remove('居民服务、修理和其他服务业')
-    if '房地产业' in result:
-        result.remove('房地产业')
     if '水利、环境和公共设施管理业' in result:
         result.remove('水利、环境和公共设施管理业')
     return result
@@ -24,20 +18,17 @@ def titleFilter(document):
     print('title')
     result = []
     similarWord = []
-    import pickle
-    basepath = os.path.abspath(__file__)
-    folder = os.path.dirname(basepath)
-    data_path = os.path.join(folder, 'origin_data')
-    f = open(data_path, 'rb')
-    category_nouns = pickle.load(f)
     title = document.title
-    for key in category_nouns:
-        nouns = [key]
-        nouns.extend(category_nouns[key])
-        for noun in nouns:
-            if noun in title:
-                result.append(key)
-                similarWord.append(noun)
+    for label in INDUSTRY_LIST:
+        category_nouns = get_industry_standard(label)
+
+        for children in category_nouns['children']:
+            nouns = [children['label']]
+            nouns.extend([x['label'] for x in children['children']])
+            for noun in nouns:
+                if noun in title:
+                    result.append(label)
+                    similarWord.append(noun)
     print(similarWord)
     return result
 
@@ -46,59 +37,55 @@ def contentFileter(document):
     # data没有取名词的操作
     result = []
     similarWord = []
-    import pickle
-    basepath = os.path.abspath(__file__)
-    folder = os.path.dirname(basepath)
-    data_path = os.path.join(folder, 'origin_data')
-    f = open(data_path, 'rb')
-    category_nouns = pickle.load(f)
     text = document.content
-    for key in category_nouns:
-        nouns = getNouns(key)
-        nouns.extend(category_nouns[key])
-        for noun in nouns:
-            if noun in text:
-                result.append(key)
-                similarWord.append(noun)
+    for label in INDUSTRY_LIST:
+        category_nouns = get_industry_standard(label)
+        for children in category_nouns['children']:
+            nouns = getNouns(children['label'])
+            nouns.extend([x['label'] for x in children['children']])
+            for noun in nouns:
+                if noun in text:
+                    result.append(label)
+                    similarWord.append(noun)
     print(similarWord)
     return result
 
-def similarFilter(document):
-    print('similar')
-    result_scores = {}
-    result=[]
-    similarWord=[]
-    import pickle
-    basepath = os.path.abspath(__file__)
-    folder = os.path.dirname(basepath)
-    data_path = os.path.join(folder, 'scores')
-    f = open(data_path, 'rb')
-    category_nouns = pickle.load(f)
-
-    text = document.content
-    for key in category_nouns:
-        score = 0
-        value = category_nouns[key]
-        for vk in value:
-            if vk in text:
-                score+=value[vk]
-                similarWord.append(vk)
-        if score ==1 :
-            score=0
-        result_scores[key] = score
-    list1 = sorted(result_scores.items(), key=lambda x: x[1], reverse=True)
-    for rs in list1:
-        if rs[1]>0.1:
-            result.append(rs[0])
-    print(similarWord)
-    return result
+# def similarFilter(document):
+#     print('similar')
+#     result_scores = {}
+#     result=[]
+#     similarWord=[]
+#     import pickle
+#     basepath = os.path.abspath(__file__)
+#     folder = os.path.dirname(basepath)
+#     data_path = os.path.join(folder, 'scores')
+#     f = open(data_path, 'rb')
+#     category_nouns = pickle.load(f)
+#
+#     text = document.content
+#     for key in category_nouns:
+#         score = 0
+#         value = category_nouns[key]
+#         for vk in value:
+#             if vk in text:
+#                 score+=value[vk]
+#                 similarWord.append(vk)
+#         if score ==1 :
+#             score=0
+#         result_scores[key] = score
+#     list1 = sorted(result_scores.items(), key=lambda x: x[1], reverse=True)
+#     for rs in list1:
+#         if rs[1]>0.1:
+#             result.append(rs[0])
+#     print(similarWord)
+#     return result
 
 def nameProcess(result):
     i=0
     while i <len(result):
         if result[i] == '农业、林业、牧业、渔业':
             result[i] = '农、林、牧、渔业'
-        if result[i] == '卫生和社会工作':
-            result[i] = ''
         i+=1
     return result
+
+
---
+++ ns_ai_system/data_management/api/industry_standard.py
@@ -0,0 +1,56 @@
+from data_management.config import py_client
+from data_management.api.field_info import get_all_field_info
+from condition_identification.args import FILTED_FIELD
+def insert_industry_standard(filtered_values):
+    """
+    保存每个数据列过滤后的值。重复的key不会覆盖。
+
+    Examples:
+    insert_filtered_values(
+        {'field': 'field_1', 'values': [1, 2, 3, 4, 5]},
+        {'field': 'field_2', 'values': [1, 2, 3, 4, 5]}
+    )
+    Args:
+        *filtered_values: 形如 {'field': 'field_1', 'values': [1, 2, 3, 4, 5]} 的字典
+
+    Returns:
+        None
+    """
+    py_client.ai_system["industry_standard"].insert_one(filtered_values)
+
+def delete_industry_standard(label):
+    """
+    删除某个数据列过滤后的值
+
+    Examples:
+            delete_filtered_values("field_1")
+
+    Args:
+        field: 数据列列名
+
+    Returns:
+        None
+    """
+    py_client.ai_system["industry_standard"].delete_one({"label": label})
+
+def get_industry_standard(label):
+    """
+    获取某个数据列过滤后的值
+
+    Examples:
+            get_filtered_values("field_1")
+
+    Args:
+        field: 数据列列名
+
+    Returns:
+        如果没有则返回None
+
+        Examples:
+            {'field': 'field_1', 'values': [1, 2, 3, 4, 5]}
+
+    """
+    return py_client.ai_system["industry_standard"].find_one({"label": label})
+
+
+
